3af5fee7eb63a0bc36780de5b55e6a24a0c636c2
Stefano <stefano.lissiaeu@gmail.com>
2017-03-20 17:27:15 +0000

    js ads

--- js/improved.js.1.0.js
+++ js/improved.js.1.0.js
@@ -3,6 +3,10 @@
   //new and improved formula
   var but = document.createElement('button');
   var al = document.createElement('a');
+  var agreen = document.createElement('a');
+  var sgreen = document.createElement('span');
+  var bgreen = document.createElement('a');
+  var dgreen = document.createElement('span');
   //Process
   function Process(n, t, l) {
       !function c() {
@@ -16,7 +20,10 @@
       runCss: {
           step1: 'js step 1 completed!',
           step2: 'href and button completed',
-          step3: 'appended elements to dom completed'
+          step3: 'blue cta style completed',
+          step4: 'green cta button completed',
+          step5: 'green cta styles completed',
+          step6: 'blue-green search results completed'
 
       },//runCss
       timers: {
@@ -29,6 +36,7 @@
           step6: 'js step 6 completed!',
           step1: 'css styles completed'
       },
+      
       //step7 (optional)
       other: function(name) {
           name = name || "no other functions have been added";
@@ -72,21 +80,29 @@
           style.innerHTML += ".hays-search-header {padding:0;}\n";
           style.innerHTML += "main.pl-content {background-color:#f0f0f0;}\n";
           style.innerHTML += "#search .panel-layout .pl-canvas {background-color:#f0f0f0;}\n";
-          style.innerHTML += ".hays-jobs .result-list.sponsored-jobs .result {padding-right:0;border-left:0;padding-left:0;margin-left:0;}\n";
+          style.innerHTML += ".hays-jobs .result-list.sponsored-jobs .result {padding-right:0 !important; border:0 !important; padding-left:0 !important; margin-left:0 !important; margin-top:0 !important; margin-bottom:0 !important;padding-top:18px !important;}\n";
           style.innerHTML += ".hays-jobs {margin:0;padding:0;}\n";
-          style.innerHTML += ".hays-jobs .result-list {padding:0 10px;}\n";
+          style.innerHTML += ".hays-jobs .result-list {padding:0;}\n";
           style.innerHTML += "li.result.first.last {padding:0;width:100%;overflow:auto;}\n"
-          style.innerHTML += ".result-list.sponsored-jobs {background-color:#fff !important;margin-top:0;border:1px solid #002776; padding:18px 0px 0px;}\n";
+          style.innerHTML += ".result-list.sponsored-jobs {background-color:#fff !important;margin-top:0;border:1px solid #002776 !important; padding:0px;}\n";
           style.innerHTML += ".job-search-title {padding:0 12px !important;}\n";
-          style.innerHTML += "p.value {margin-bottom:22px;}"
+          style.innerHTML += "p.value {margin-bottom:22px; color:808080 !important; font-weight:400;a}"
           style.innerHTML += ".hays-jobs .hays-result-sponsored {display:block; padding:5px 12px 5px 12px;margin:0; border:0;font-size:10px;line-height:18px;background-color:#002776;float:none !important;color:#fff !important;font-weight:normal;}\n";
           style.innerHTML += ".hays-jobs .hays-result-rate-value {margin-bottom:0;}\n";
-          style.innerHTML += ".hays-jobs .hays-result-location {margin-bottom:-5px; padding-left:12px;}\n";
-          style.innerHTML += ".hays-jobs .hays-result-location .value {margin-left:5px;line-height:10px;font-size14px;margin-bottom:20px;}\n";
+          style.innerHTML += ".hays-jobs .hays-result-location {margin-bottom:0px; padding-left:12px;}\n";
+          style.innerHTML += ".hays-jobs .hays-result-location .value {margin-left:5px;line-height:12px;font-size:12px;margin-bottom:5px;font-weight:400;}\n";
           style.innerHTML += "field hays-result-title  jobtitle tk-stl-title {margin-bottom:20px;}\n";
           style.innerHTML += ".hays-jobs .hays-result-description {margin:0; max-width:none; max-width:none; max-height:none;font-size:14px;vertical-align:middle;padding:0 12px !important;}\n";
-          style.innerHTML += ".hays-jobs .job-search-title {margin-bottom:10px;}\n";
-          style.innerHTML += ".field.hays-result-rate-value {width:45%; padding-left:12px; line-height:18px;}\n";
+          style.innerHTML += ".hays-jobs .job-search-title {margin-bottom:25px;}\n";
+          style.innerHTML += ".field.hays-result-rate-value {width:45%; padding-left:12px; line-height:14px;font-size:13px;}\n";
+          style.innerHTML += ".hays-jobs .hays-result-title .value {margin-top:0 !important;}";
+          style.innerHTML += ".hays-jobs .result-list .result { padding:0; margin:20px auto !important; border:1px solid #e6e6e6; padding-bottom:25px; padding-top:20px;}";
+          style.innerHTML += ".hays-jobs ol.result-list {border:0; width:95%; margin:0 auto;}";
+          style.innerHTML += ".hays-jobs .result-list .result.first {}";
+          style.innerHTML += ".result-list.sponsored-jobs {}";
+         
+
+
 
 
 
@@ -97,36 +113,93 @@
 
       },
       function step2(next) {
-         debugger;
+         //debugger;
          al.className = 'adeet';
          but.className = 'butdetails';
          al.textContent = 'View details';
-         //apend to dom
          but.appendChild(al);
          //append to dom
          var doe = document.querySelector('.field.hays-result-rate-value');
          doe.appendChild(but);
          console.log('step 2 :' + this.runCss.step2);
-         next()
+         next();
          
       },
 
-      //cta styles
-      function step3() {      
+      //cta blue styles
+      function step3(next) {      
           if (!undefined) {
                 but.style.position = 'absolute';
-                but.style.right = '5%';
-                but.style.bottom = '22%';
-                but.style.padding = '14px 35px';
+                but.style.right = '4%';
+                but.style.bottom = '25%';
+                but.style.padding = '9px 21px';
                 but.style.backgroundColor = '#009fda';
                 but.style.border = '0';
                 but.style.boxShadow = '0px 0px 1px #0479a5';
                 al.style.fontSize = '14px';
-                al.style.lineHeight = '22px';
+                al.style.lineHeight = '24px';
                 al.style.color = '#ffffff';   
           } 
+          console.log('step 3 :' + this.runCss.step3);
+          next();
+      },
 
-      }              
+      function step4(next) {
+          debugger;
+          agreen.className = 'agreen';
+          sgreen.className = 'agreen';
+          agreen.textContent = 'NEW';
+          sgreen.appendChild(agreen);
+          //append to dom
+          var daa = document.querySelector('.field.hays-result-title');
+          daa.appendChild(sgreen);
+          console.log('step 4 :' + this.runCss.step4);
+          next();
+      } ,
+
+      //cta green styles
+      function step5() {      
+          if (!undefined) {
+                sgreen.style.position = 'absolute';
+                sgreen.style.top = '1%';
+                sgreen.style.left = '105%';
+                sgreen.style.border = '1px solid #afc350';
+                agreen.style.padding = '5px 3px';
+                agreen.style.fontSize = '12px';
+                agreen.style.lineHeight = '18px';
+                agreen.style.color = '#afc350';   
+          } 
+          console.log('step 5 :' + this.runCss.step5);
+          
+      }
+
+      /*
+      function step6() {   
+
+          bgreen.className = 'bgreen';
+          dgreen.className = 'dgreen';
+          bgreen.textContent = 'NEW';
+          dgreen.appendChild(agreen);
+          //append to dom
+          var ddd = document.querySelectorAll('.job-search-title')[1];
+          ddd.appendChild(dgreen);
+          console.log('step 4 :' + this.runCss.step4);
+          if (!undefined) {
+                dgreen.style.position = 'absolute';
+                dgreen.style.top = '1%';
+                dgreen.style.left = '105%';
+                dgreen.style.border = '1px solid #afc350';
+                bgreen.style.padding = '5px 3px';
+                bgreen.style.fontSize = '12px';
+                bgreen.style.lineHeight = '18px';
+                bgreen.style.color = '#afc350';   
+          } 
+          console.log('step 6 :' + this.runCss.step6);
+         
+
+      },*/
+
+
   ],//Process:Init
   
   function done() {
@@ -144,18 +217,18 @@
   //Process:Init.runCode
   setTimeout(function() {
       Process(Init.runCode, [
-        function step4(next) {
+        function step5(next) {
             console.log(this.step4);
             next();
         },
-        function step5(next) {
+        function step6(next) {
             var self1 = this, arg;
             setTimeout(function() {
                 next(arg);
             }, 200);
             console.log(this.step5);
         },
-        function step6(arg, next) {
+        function step7(arg, next) {
             arg = arg || 'noname';
             console.log(arg);
             console.log(this.step6);
