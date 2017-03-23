f3e893b4d078070c41c7a94de6f6593ba3e15878
Stefano <stefano.lissiaeu@gmail.com>
2017-03-23 19:05:41 +0000

    hays code revised

--- js/j1.js
+++ js/j1.js
@@ -15,7 +15,7 @@ function checkifbodyexist() {
             checkifbodyexist();
         }, 10);
     } else {
-        document.body.style.opacity = 0;
+        //document.body.style.opacity = 0;
     }
 }
 checkifbodyexist();
@@ -30,8 +30,7 @@ checkifbodyexist();
             this.mainCss(true);
             this.addClass();
             this.ajaxComplete();
-            this.addNew();
-            this.blackPound();
+            this.attachToP();
 
         },
         mainCss: function(applyCss) {
@@ -59,11 +58,11 @@ checkifbodyexist();
                 style.innerHTML += "p.value {margin-bottom:22px; color:808080 !important; font-weight:400;font-size:14px;line-height:16px;}";
                 style.innerHTML += ".hays-jobs .hays-result-sponsored {width:20%;margin-left:12px!important; text-align:center; margin-bottom:15px !important; display:block; padding:5px 12px 5px 12px;margin:0; border:0;font-size:10px;line-height:20px;background-color:#002776;float:none !important;color:#fff !important;font-weight:normal;cursor:pointer;letter-spacing:.25px;}\n";
                 style.innerHTML += ".hays-jobs .hays-result-rate-value {margin-bottom:0;}\n";
-                style.innerHTML += ".hays-jobs .hays-result-location {margin-bottom:0px; padding-left:12px;}\n";
+                style.innerHTML += ".hays-jobs .hays-result-location {margin-bottom:0px; padding-left:12px;font-size:15px;}\n";
                 style.innerHTML += ".hays-jobs .hays-result-location .value {margin-left:5px;line-height:16px;font-size:14px;margin-bottom:5px;font-weight:400;color:#808080 !important;}\n";
                 style.innerHTML += "field hays-result-title  jobtitle tk-stl-title {margin-bottom:20px;}\n";
                 style.innerHTML += ".hays-jobs .hays-result-description {margin:0; max-width:none; max-width:none; max-height:none;font-size:14px;vertical-align:middle;padding:0 12px !important;}\n";
-                style.innerHTML += ".hays-jobs .job-search-title {margin-bottom:25px; display:inline-block;}\n";
+                style.innerHTML += ".hays-jobs .job-search-title {margin-bottom:30px; display:inline-block;}\n";
                 style.innerHTML += ".field.hays-result-rate-value {width:45%; padding-left:12px; line-height:14px;font-size:13px;}\n";
                 style.innerHTML += ".hays-jobs .hays-result-title .value {margin-top:0 !important; margin-right:1em;}";
                 style.innerHTML += ".hays-jobs .result-list .result { padding:0; padding-bottom:30px !important; margin:20px auto !important; border:1px solid #e6e6e6; padding-bottom:0; padding-top:20px; background-color:#ffffff;}";
@@ -83,18 +82,29 @@ checkifbodyexist();
                 style.innerHTML += "div#pageTop1.pageNo.pageCurrent {background-color:#f0f0f0;}";
                 style.innerHTML += ".hays-jobs {background-color:#fff;}";
                 style.innerHTML += ".panel-layout .pl-content {background-color:#f0f0f0 !important;}";
-                style.innerHTML += ".hays-jobs a.butdetails {position: absolute;right: 4%;bottom: 13%;padding: 10px 28px;}";
+                style.innerHTML += ".hays-jobs a.butdetails {position: absolute;right: 4%;bottom: 22%;padding: 10px 28px;}";
+                style.innerHTML += "a.butdetails.hover,a.butdetails:active {background-color:#002776;text-decoration:none;}";
 
-                style.innerHTML += ".hays-jobs .sponsored-jobs a.butdetails {		 position: absolute;right: 4%;bottom: 27%;padding: 10px 28px;background-color: rgb(0, 159, 218); border: 0px;box-shadow: rgb(4, 121, 165) 0px 0px 1px;color: #fff !important;font-size: 15px;line-height: 22px;vertical-align: middle;cursor: pointer;letter-spacing: .25px;}";
+                style.innerHTML += ".hays-jobs .sponsored-jobs a.butdetails {		 position: absolute;right: 4%;bottom: 32%;padding: 10px 28px;background-color: rgb(0, 159, 218); border: 0px;box-shadow: rgb(4, 121, 165) 0px 0px 1px;color: #fff !important;font-size: 15px;line-height: 22px;vertical-align: middle;cursor: pointer;letter-spacing: .25px;}";
                 style.innerHTML += "#search .pageNo {background-color:#f0f0f0 !important;font-weight: 500;color: #8a8b8d;font-size: 18px;padding: 3px;}";
                 style.innerHTML += "p.value {::first-letter:color:#black;}";
-                style.innerHTML += ".blackPound::first-letter {color:#ffffff;font-size:14px;margin-right:4px; border-radius:50%;letter-spacing:6px;text-align:center;padding:0 0px 0px 3px;background-color:#313131;}";
+                style.innerHTML += ".blackPound::first-letter {color:#ffffff;font-size:14px;margin-right:5px;margin-left:-2px; border-radius:50%;letter-spacing:6px;text-align:center;padding:2px 0px 2px 5px;background-color:#313131; display:inline-block;}";
                 style.innerHTML += ".hays-result-sponsored img {right:9999px !important;}";
                 style.innerHTML += ".hays-jobs .result-list .last {padding-bottom:1em !important;}";
 
                 style.innerHTML += ".hays-jobs .hays-result-sponsored img {display:none !important;}";
 
                 style.innerHTML += ".hays-jobs .result-list .result img {right:99999px !important;}";
+                style.innerHTML += ".agreen {display:inline-block;font-size:12px;vertical-align:middle; line-height:15px;text-align:center;vertical-align:bottom;color:#afc350 !important;background-color:#fff !important;border:1px solid rgb(175,195,80); padding:2px 3px; font-weight:normal;position: relative; top: 2px;}";
+
+                style.innerHTML += ".field.hays-result-location.xlocationdescription {width:49%;}"
+                style.innerHTML += "p.value.blackPound:before {position:absolute;left:0; top:0;}";
+                style.innerHTML += "field.hays-result-rate-value p.value::before {margin-left:-5px;}";
+                style.innerHTML += "field.hays-result-rate-value p.value {margin-left:0px;}";
+                style.innerHTML += ".hays-jobs .hays-result-title .value a {padding-right:10px; }";
+                style.innerHTML += ".blackPound {display:inline-block;}"
+
+
 
                 //append to head
                 document.getElementsByTagName('head')[0].appendChild(style);
@@ -111,82 +121,61 @@ checkifbodyexist();
         addClass: function() {
 
 
-            document.querySelectorAll('.hays-jobs .result-list .result').forEach(function(el) {
-                if (hasClass(el, "changes-done")) {
+            $('.hays-jobs .result-list .result').each(function(i) {
+                var el = $(this);
+                if ($(this).hasClass('changes-done')) {
                     // CTA HAS BEEN ADDED ALREADY'.l..s
                 } else {
-                    el.className += ' changes-done';
+                    el.addClass('changes-done');
 
-                    var link = el.querySelector('.job-search-title a');
+                    /* append pound */
+                    $(this).find('.field.hays-result-rate-value p.value').prepend('<span class="blackPound">£</span>');
+
+                    /* new */
+                    if($(this).find('.job-search-title span').text() == 'NEW'){
+                        var nnew = $(this).find('.job-search-title span');
+                        var title = $(this).find('.job-search-title p.value');
+                        $(nnew).appendTo(title);
+                        $(nnew).addClass('agreen');
+                    }
+                    // $(this).find('.job-search-title span').addClass('agreen').text("NEW");
+
+
+                    /* main CTA */
+                    var link = el.find('.job-search-title a').attr('href');
 
                     var a = document.createElement('a');
                     var linkText = document.createTextNode("View details");
                     a.appendChild(linkText);
                     a.title = "View details";
                     a.className = 'butdetails';
-                    a.href = link.href;
+                    a.href = link;
 
-
-                    el.querySelector('.field.hays-result-rate-value').appendChild(a);
+                    el.find('.field.hays-result-rate-value').append(a);
 
                 }
             });
         },
-        addNew: function() {
-            //debugger;
-            //sponsored and new have no links
-            document.querySelectorAll('.job-search-title span').forEach(function(el) {
-                if (hasClass(el, "changes-done")) {
 
-                } else {
-                    //add class here too
-                    el.className = 'agreen';
-                    el.textContent = 'NEW';
-                    el.style.display = 'inline-block';
-                    el.style.fontSize = '12px';
-                    el.style.lineHeight = '18px';
-                    el.style.textAlign = 'center';
-                    el.style.verticalAlign = 'bottom';
-                    el.style.fontSize = '12px';
-                    el.style.lineHeight = '18px';
-                    el.style.color = '#afc350';
-                    el.style.backgroundColor = '#ffffff';
-                    el.style.border = '1px solid rgb(175, 195, 80)';
-                    el.style.padding = '0 3px';
+        attachToP: function() {
+              
+               
+                // $('.field.hays-result-title.new-job').each(function() {
+                //         //grab handles
+                //         var n = $(this).find('p.value');
+                //         var spano = this.parentElement.children[1];
+                //         //append
+                //         n.append(spano);
 
 
-
-                }
-
-
-
-            });
-        },
-
-        blackPound: function() {
-            //debugger;
-           
-            document.querySelectorAll('.field.hays-result-rate-value').forEach(function(el) {
-
-                if (el.textContent.charAt(0) == '£') {
-                    //console.log(el.textContent.charAt(0));
-                    //p::first-letter {color:black;}
-                    el.classList.add('blackPound');
-
-                } else {
-                    //console.log('do nothing');
-                    //console.log(el.textContent.charAt(0));
-
-                }
-
-
-
-            });
+                        
+                     
+                // });
 
 
 
         },
-
+        
 
 
         ajaxComplete: function() {
@@ -208,15 +197,12 @@ checkifbodyexist();
         Hays01.init();
 
         setTimeout(function() {
-            document.body.style.opacity = 1;
+            //document.body.style.opacity = 1;
         }, 500);
 
 
-        // } catch(err){
-        //   document.body.style.opacity = 1;
-        //   console.log('Something happen: '+ err);
-        // }
+        
 
     });
 
-})(); // This is just a sample script. Paste your real code (javascript or HTML) here.
+})(); 
